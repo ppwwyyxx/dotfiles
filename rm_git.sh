15d33b2c9e4052cc8f506b946839a17e5d4b550e
ppwwyyxx <ppwwyyxxc@gmail.com>
2012-11-05 16:21:21 +0800

    add latex-box, fix surround & indent-object

--- rm_git.sh
+++ rm_git.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # File: rm_git.sh
-# Date: Sun Nov 04 09:23:22 2012 +0800
+# Date: Mon Nov 05 16:21:12 2012 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 mv `find .vim |grep 'git$' |tr '\n' ' '` ~/.Trash/ --backup=numbered -f
+mv `find .vim |grep '\.gitignore$' |tr '\n' ' '` ~/.Trash/ --backup=numbered -f
