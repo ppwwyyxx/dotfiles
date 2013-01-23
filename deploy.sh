ddde671c24dea54c17e4e2090a0ca1e45d698b23
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-01-23 23:34:15 +0800

    fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # File: deploy.sh
-# Date: Mon Jan 21 10:32:35 2013 +0800
+# Date: Wed Jan 23 23:25:15 2013 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
-rm ~/.vim ~/.zsh
+rm ~/.vim ~/.zsh -rf
 cp .vim .zsh .vimrc .zshrc .aliasrc ~/ -rvf
