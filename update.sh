122a6877fdf386daa8dcb57b2b10ef955fb437f4
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-04-14 01:27:08 +0800

    test

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # File: update.sh
-# Date: Fri Mar 01 10:47:40 2013 +0800
+# Date: Thu Apr 11 20:38:32 2013 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 if [[ ! "$HOST" -eq "KeepMoving" ]]; then
 	echo "Not at KeepMoving! "
@@ -9,7 +9,7 @@ fi
 rm .vimrc .vim -rfv
 cp ~/.vimrc ~/.vim ./ -rvH
 rm .zshrc .aliasrc .zsh -rfv
-cp ~/.zshrc ~/.aliasrc ~/.zsh ./ -rv
+cp ~/.zshrc ~/.aliasrc ~/.zsh ./ -rvH
 rm `find .vim |egrep 'git$|svn$'`  -rvf
 rm `find .vim |egrep '\.gitignore$'` -rvf
 rm ../vim.7z
