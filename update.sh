8899f188d4730e80ff060b7933a17b18ab2d63dd
ppwwyyxx <ppwwyyxxc@gmail.com>
2013-07-04 01:51:35 +0800

    add ycm!

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # File: update.sh
-# Date: Fri May 10 18:39:05 2013 +0800
+# Date: Thu Jul 04 01:51:06 2013 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 if [[ ! "$HOST" -eq "KeepMoving" ]]; then
 	echo "Not at KeepMoving! "
@@ -12,6 +12,6 @@ rm .zshrc .aliasrc .zsh -rfv
 cp ~/.zshrc ~/.aliasrc ~/.zsh ./ -rvH
 rm `find .vim |egrep 'git$|svn$'`  -rvf
 rm `find .vim |egrep '\.gitignore$'` -rvf
-rm ../vim.7z
-7z a ../vim.7z /ssd_home/wyx/.vim
+#rm ../vim.7z
+#7z a ../vim.7z /ssd_home/wyx/.vim
 
