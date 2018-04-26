43b980b34c291a1eac88b3c670cf9c0bdf1976a1
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-04-25 18:46:09 -0700

    action map

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -16,50 +16,48 @@
   (evil-ex-define-cmd "vsp" 'evil-window-vsplit)
 )
 
-
 ;(ivy-add-actions 'ivy-switch-buffer '(("z" switch-to-buffer-other-window "test")))
 ;(ivy-add-actions 'counsel-find-file' (("z" switch-to-buffer-other-window "test")))
 (defun switch-to-buffer-new-window (buffer-or-name side)
   "Switch to BUFFER-OR-NAME in a new window, based on SIDE."
+  ;(sleep-for 5)
   (select-window (split-window nil nil side))
   (balance-windows)
-  (switch-to-buffer buffer-or-name))
+  (switch-to-buffer buffer-or-name)
+  )
 
-(defun find-file-new-window (filename)
-  ;(switch-to-buffer-new-window (find-file-noselect (projectile-expand-root filename)) 'right)
-  (switch-to-buffer-new-window (find-file-noselect filename) 'right)
-)
+(defun find-file-new-window (filename side)
+  (switch-to-buffer-new-window (find-file-noselect filename) side))
+
+;(load "/home/yuxinwu/test")
 
 (after! ivy
-    (defun ivy-exit-buffer-with-new-window (side)
-		(ivy-exit-with-action
-          (lambda (buffer-or-name) (switch-to-buffer-new-window buffer-or-name side)))
-	)
-
-	(defun ivy-exit-file-with-new-window (side)
-		(ivy-exit-with-action
-          (lambda (filename) (switch-to-buffer-new-window (find-file-noselect filename) side))
-        )
-	)
-
-    (defun ivy-exit-map-test ()
-      (interactive)
-      (let ((find-file-other-window 'find-file-new-window))
-	    (ivy-exit-with-action 'counsel-projectile-find-file-action-other-window)
-      )
-      ;(ivy-set-action 'ivy-exit-file-action-test)
-      ;(message (format "Act=%s" (ivy-state-action ivy-last)))
-      ;(ivy-set-action 'find-file-other-window)
-      ;(let ((find-file-other-window 'find-file-new-window))
-      ;  (ivy-done))
+    (defun my/projectile-find-file-new-window (filename side)
+      (find-file-new-window (projectile-expand-root filename) side)
+      (run-hooks 'projectile-find-file-hook)
     )
-	(define-key ivy-switch-buffer-map (kbd "C-v") (lambda! (ivy-exit-buffer-with-new-window 'right)))
-	(define-key ivy-switch-buffer-map (kbd "C-s") (lambda! (ivy-exit-buffer-with-new-window 'below)))
-	(define-key ivy-switch-buffer-map (kbd "C-x") (lambda! (ivy-exit-buffer-with-new-window 'below)))
-	;(define-key ivy-minibuffer-map (kbd "C-v") (lambda! (ivy-exit-file-with-new-window 'right)))
-	(define-key ivy-minibuffer-map (kbd "C-v") 'ivy-exit-map-test)
-	(define-key ivy-minibuffer-map (kbd "C-s") (lambda! (ivy-exit-file-with-new-window 'below)))
-	(define-key ivy-minibuffer-map (kbd "C-x") (lambda! (ivy-exit-file-with-new-window 'below)))
+
+    (defvar my/ivy-new-window-action-map (make-hash-table)
+    "Map from ivy actions (symbols) to actions which take an extra side argument.")
+    (puthash 'counsel-projectile-find-file-action 'my/projectile-find-file-new-window my/ivy-new-window-action-map)
+    ;(puthash 'counsel-projectile-find-file-action 'newfunc my/ivy-new-window-action-map)
+    (puthash 'ivy--switch-buffer-action 'switch-to-buffer-new-window my/ivy-new-window-action-map)
+    ;(puthash 'counsel-git-grep-action 'newfunc my/ivy-new-window-action-map)
+
+    (defun my/ivy-exit-new-window (side)
+      (let ((current-act (ivy--get-action ivy-last)))
+        (message "current-act=%s" (type-of current-act))
+        (message "current-act=%s" current-act)
+        (cl-flet ((side-action (gethash current-act my/ivy-new-window-action-map
+                                        (lambda (x _) (funcall current-act x)))))
+            (ivy-exit-with-action (lambda (filename) (side-action filename side)))
+        )
+      )
+    )
+
+	(define-key ivy-minibuffer-map (kbd "C-v") (lambda! (my/ivy-exit-new-window 'right)))
+	(define-key ivy-minibuffer-map (kbd "C-s") (lambda! (my/ivy-exit-new-window 'below)))
+	(define-key ivy-minibuffer-map (kbd "C-x") (lambda! (my/ivy-exit-new-window 'below)))
 )
 
 (setq doom-font (font-spec :family "Monospace" :size 20))
