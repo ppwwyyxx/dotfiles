6b97ef6485d8595f80dee950613933449f2cd052
Yuxin Wu <ppwwyyxxc@gmail.com>
2018-04-26 18:18:22 -0700

    some update

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,61 +1,69 @@
 ;;; -*- lexical-binding: t -*-
 ;;; -*- no-byte-compile: t -*-
-;(use-package evil-collection
-;  :after evil
-;  :custom (evil-collection-setup-minibuffer t)
-;  :init (evil-collection-init))
 
-
-;(def-package-hook! evil
-;  :pre-init
-;  (setq evil-magic nil)
-;  t)
 (global-set-key (kbd "C-x C-b") 'ibuffer)
 
 (after! evil
+  ; https://github.com/hlissner/doom-emacs/issues/552
+  (setq evil-split-window-below t)
+  (setq evil-vsplit-window-right t)
   (evil-ex-define-cmd "vsp" 'evil-window-vsplit)
 )
 
-;(ivy-add-actions 'ivy-switch-buffer '(("z" switch-to-buffer-other-window "test")))
-;(ivy-add-actions 'counsel-find-file' (("z" switch-to-buffer-other-window "test")))
-(defun switch-to-buffer-new-window (buffer-or-name side)
-  "Switch to BUFFER-OR-NAME in a new window, based on SIDE."
-  ;(sleep-for 5)
-  (select-window (split-window nil nil side))
-  (balance-windows)
-  (switch-to-buffer buffer-or-name)
-  )
-
-(defun find-file-new-window (filename side)
-  (switch-to-buffer-new-window (find-file-noselect filename) side))
-
-;(load "/home/yuxinwu/test")
-
 (after! ivy
-    (defun my/projectile-find-file-new-window (filename side)
-      (find-file-new-window (projectile-expand-root filename) side)
-      (run-hooks 'projectile-find-file-hook)
-    )
+  (map! :n "C-p" #'counsel-projectile-find-file)
+  ;(ivy-add-actions 'ivy-switch-buffer '(("z" switch-to-buffer-other-window "test")))
+  ;(ivy-add-actions 'counsel-find-file' (("z" switch-to-buffer-other-window "test")))
 
-    (defvar my/ivy-new-window-action-map (make-hash-table)
-    "Map from ivy actions (symbols) to actions which take an extra side argument.")
-    (puthash 'counsel-projectile-find-file-action 'my/projectile-find-file-new-window my/ivy-new-window-action-map)
-    ;(puthash 'counsel-projectile-find-file-action 'newfunc my/ivy-new-window-action-map)
-    (puthash 'ivy--switch-buffer-action 'switch-to-buffer-new-window my/ivy-new-window-action-map)
-    ;(puthash 'counsel-git-grep-action 'newfunc my/ivy-new-window-action-map)
+  ;(defun switch-to-buffer-new-window (buffer-or-name side)
+  ;  "Switch to BUFFER-OR-NAME in a new window, based on SIDE."
+  ;  (select-window (split-window nil nil side))
+  ;  (balance-windows)
+  ;  (switch-to-buffer buffer-or-name)
+  ;  )
 
-    (defun my/ivy-exit-new-window (side)
-      (let ((current-act (ivy--get-action ivy-last)))
-        (message "current-act=%s" (type-of current-act))
-        (message "current-act=%s" current-act)
-        (cl-flet ((side-action (gethash current-act my/ivy-new-window-action-map
-                                        (lambda (x _) (funcall current-act x)))))
-            (ivy-exit-with-action (lambda (filename) (side-action filename side)))
+  ;(defun find-file-new-window (filename side)
+  ;  (switch-to-buffer-new-window (find-file-noselect filename) side))
+
+  ;(defmacro my/new-window-context (code side)
+  ;  `(letf (
+  ;                                      ;((symbol-function 'find-file-other-window) (lambda (a) (find-file-new-window a ,side)))
+  ;          ((symbol-function 'find-file) (lambda (a) (find-file-new-window a ,side)))
+  ;                                      ;((symbol-function 'switch-to-buffer-other-window) (lambda (a) (switch-to-buffer-new-window a ,side)))
+  ;          )
+  ;     ,code
+  ;     )
+  ;  )
+
+  (defun my/ivy-exit-new-window (side)
+    ;(cl-flet ((current-act (ivy--get-action ivy-last)))
+    ;  (ivy-exit-with-action
+    ;    (lambda (x)
+    ;      (message "WITH %s" x)
+    ;      (select-window (split-window nil nil side))
+    ;      (balance-windows)
+    ;      (current-act x)
+    ;    ))
+    ;)
+
+    (let ((current-act (ivy--get-action ivy-last)))
+      (pcase current-act
+        ; TODO handle special cases
+        (-
+         (ivy-exit-with-action
+          (lambda (x)
+            (setf
+             (ivy-state-window ivy-last)
+             (select-window (split-window nil nil side))
+            )      ; so that with-ivy-window (used a lot inside predefined actions) will use the new window
+            (balance-windows)
+            (funcall current-act x)
+            )))
         )
       )
     )
 
-	(define-key ivy-minibuffer-map (kbd "C-v") (lambda! (my/ivy-exit-new-window 'right)))
+    (define-key ivy-minibuffer-map (kbd "C-v") (lambda! (my/ivy-exit-new-window 'right)))
 	(define-key ivy-minibuffer-map (kbd "C-s") (lambda! (my/ivy-exit-new-window 'below)))
 	(define-key ivy-minibuffer-map (kbd "C-x") (lambda! (my/ivy-exit-new-window 'below)))
 )
@@ -63,3 +71,4 @@
 (setq doom-font (font-spec :family "Monospace" :size 20))
 (set-face-attribute 'line-number-current-line nil :inherit 'default)
 ;; https://github.com/hlissner/emacs-doom-themes/blob/master/doom-themes-common.el#L67-L70
+
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -18,7 +18,6 @@
 
        :completion
        company           ; the ultimate code completion backend
-       ;helm              ; the *other* search engine for love and life
       ;ido               ; the other *other* search engine...
        ivy               ; a search engine for love and life
 
@@ -30,7 +29,7 @@
        evil-goggles      ; display visual hints when editing in evil
        hl-todo           ; highlight TODO/FIXME/NOTE tags
        nav-flash         ; blink the current line after jumping
-      ;posframe          ; use child frames where possible (Emacs 26+ only)
+       posframe          ; use child frames where possible (Emacs 26+ only)
       ;tabbar            ; FIXME an (incomplete) tab bar for Emacs
       ;unicode           ; extended unicode support for various languages
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
