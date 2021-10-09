c50e763ac87910176786111dced1c2d7c471d73c
Yuxin Wu <ppwwyyxxc@gmail.com>
2021-10-08 19:11:09 -0700

    update

--- update-kde.sh
+++ update-kde.sh
@@ -3,6 +3,16 @@
 # https://github.com/shalva97/kde-configuration-files
 for i in plasma-org.kde.plasma.desktop-appletsrc \
   kcminputrc kdeglobals kscreenlockerrc ksplashrc kwinrc plasmarc \
-  kwinrulesrc kglobalshortcutsrc baloofilerc latte lattedockrc breezerc Kvantum; do
+  kwinrulesrc kglobalshortcutsrc baloofilerc latte lattedockrc breezerc \
+  Kvantum; do
   cp -rv "$HOME/.config/$i" ./.config
 done
+
+dir=$(pwd)
+
+AR_NAME=$dir/kde-local-share.tgz
+rm $AR_NAME || true
+
+cd $HOME/.local/share
+tar czvf $AR_NAME \
+  aurorae color-schemes plasma wallpapers icons
