a944728c65ec6066c3acb3446360709110744925
Yuxin Wu <ppwwyyxxc@gmail.com>
2021-12-22 17:43:18 -0800

    update osx

--- macOS/osx-brew-install.sh
+++ macOS/osx-brew-install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install gnu-tar gnu-find
 brew install reattach-to-user-namespace tmux
-brew install wget curl cmake htop
+brew install wget curl cmake htop the_silver_searcher
 
 #brew install cocoapods
 #brew tap iina/homebrew-mpv-iina
