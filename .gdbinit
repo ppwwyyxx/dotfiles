3d999fe752f0b468aa54573461511a1bfd4376e5
Yuxin Wu <ppwwyyxx@users.noreply.github.com>
2023-10-20 14:38:04 -0700

    update

--- .gdbinit
+++ .gdbinit
@@ -3,7 +3,7 @@ import sys, os
 sys.path.insert(0, os.path.expanduser('~/.gdb'))
 end
 # python import imshow
-# python import libpython
+python import libpython
 
 set history save on
 set history size unlimited
--- macOS/osx-brew-install.sh
+++ macOS/osx-brew-install.sh
@@ -2,10 +2,13 @@
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-brew install coreutils gnu-tar gnu-find
-brew install reattach-to-user-namespace tmux
-brew install wget curl cmake htop the_silver_searcher p7zip
+brew install coreutils gnu-tar findutils gawk grep gnu-tar gnu-sed
+brew install reattach-to-user-namespace tmux neovim
+brew install wget curl cmake htop the_silver_searcher p7zip git-delta
 
+
+brew install kitty obsidian zotero visual-studio-code telegram-desktop brew/cask/megasync tunnelblick
 #brew install cocoapods
 #brew tap iina/homebrew-mpv-iina
 #brew install mpv-iina
+# Zeal: https://github.com/zealdocs/zeal/wiki/Build-Instructions-for-macOS
